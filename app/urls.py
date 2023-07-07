96dceabf72396b4e566d0643020132ace7202fe3
MiguelGaticaBestnickname123 <mi.gatica@duocuc.cl>
2023-07-07 05:14:56 -0400

    Url y view actualizado

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import inicio, loginsexo
+from .views import inicio, loginsexo, tienda
 
 
 urlpatterns = [
     path('', inicio, name="inicio"),
-    path('login/', loginsexo, name="login"),    
+    path('login/', loginsexo, name="login"), 
+    path('tienda', tienda, name="tienda"),   
 
 ]    
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import permission_required, login_required
 from .forms import LoginForm
+from .models import Plumbus
 # Create your views here.
 @login_required
 def inicio(request):
@@ -22,3 +23,7 @@ def loginsexo(request):
                 login(request, user)
                 return redirect('inicio')
     return render(request, 'app/registration/login.html', contexto)
+
+def tienda(request):
+    plumbus = Plumbus.objects.all()
+    return render (request, 'app/tienda.html', plumbus)
